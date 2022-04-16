9e4f60bf4e45e875fdb6e73e059f1d7c29ae118a
syimyuzya <syimyuzya@gmail.com>
2022-04-17 00:16:34 +0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd ../qieyun-examples > /dev/null
+python build.py > index.js
+npm pack
+commit_hash="$(git rev-parse --short HEAD)"
+
+popd > /dev/null
+tar xvf ../qieyun-examples/qieyun-examples-*.tgz
+mv package/* .
+rm -d package
+git commit -a -m "Dist for dev-qieyun-0.13 ${commit_hash}"
