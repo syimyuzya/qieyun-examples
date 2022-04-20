66655e737d586cb49c565c801b0dad85a5f1bbe3
syimyuzya <syimyuzya@gmail.com>
2022-04-20 16:23:35 +0800

    Dist for dev-qieyun-0.13 09bbad8

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ commit_hash="$(git rev-parse --short HEAD)"
 
 popd > /dev/null
 tar xvf ../qieyun-examples/qieyun-examples-*.tgz
+rm -rf test/
 mv package/* .
 rm -d package
 git commit -a -m "Dist for dev-qieyun-0.13 ${commit_hash}"
